4ea7515241248dbd7b59b457e0a90200092aa225
nenoNaninu <neno@melonpan.pro>
2022-05-31 01:51:52 +0900

    update samples

--- samples/console.typescript/generated/TypedSignalR.Client/App.Interfaces.Chat.ts
+++ samples/console.typescript/generated/TypedSignalR.Client/App.Interfaces.Chat.ts
@@ -1,15 +1,43 @@
 import { Message } from '../App.Interfaces.Chat';
 
 export type IChatHub = {
+    /**
+    * @param username Transpied from string
+    * @returns Transpied from System.Threading.Tasks.Task
+    */
     join(username: string): Promise<void>;
+    /**
+    * @returns Transpied from System.Threading.Tasks.Task
+    */
     leave(): Promise<void>;
+    /**
+    * @returns Transpied from System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<string>>
+    */
     getParticipants(): Promise<string[]>;
+    /**
+    * @param message Transpied from string
+    * @returns Transpied from System.Threading.Tasks.Task
+    */
     sendMessage(message: string): Promise<void>;
 }
 
 export type IChatReceiver = {
+    /**
+    * @param message Transpied from App.Interfaces.Chat.Message
+    * @returns Transpied from System.Threading.Tasks.Task
+    */
     onReceiveMessage(message: Message): Promise<void>;
+    /**
+    * @param username Transpied from string
+    * @param dateTime Transpied from System.DateTime
+    * @returns Transpied from System.Threading.Tasks.Task
+    */
     onLeave(username: string, dateTime: (Date | string)): Promise<void>;
+    /**
+    * @param username Transpied from string
+    * @param dateTime Transpied from System.DateTime
+    * @returns Transpied from System.Threading.Tasks.Task
+    */
     onJoin(username: string, dateTime: (Date | string)): Promise<void>;
 }
 
--- samples/console.typescript/generated/TypedSignalR.Client/App.Interfaces.WhiteBoard.ts
+++ samples/console.typescript/generated/TypedSignalR.Client/App.Interfaces.WhiteBoard.ts
@@ -1,13 +1,39 @@
 import { ShapeType } from '../App.Interfaces.WhiteBoard';
 
 export type IWhiteboardHub = {
+    /**
+    * @param shapeType Transpied from App.Interfaces.WhiteBoard.ShapeType
+    * @param x Transpied from float
+    * @param y Transpied from float
+    * @returns Transpied from System.Threading.Tasks.Task<System.Guid>
+    */
     add(shapeType: ShapeType, x: number, y: number): Promise<string>;
+    /**
+    * @param shapeId Transpied from System.Guid
+    * @returns Transpied from System.Threading.Tasks.Task
+    */
     move(shapeId: string): Promise<void>;
+    /**
+    * @param x Transpied from float
+    * @param y Transpied from float
+    * @returns Transpied from System.Threading.Tasks.Task
+    */
     writeLine(x: number, y: number): Promise<void>;
 }
 
 export type IWhiteboardReceiver = {
+    /**
+    * @param shapeId Transpied from System.Guid
+    * @param x Transpied from float
+    * @param y Transpied from float
+    * @returns Transpied from System.Threading.Tasks.Task
+    */
     onMove(shapeId: string, x: number, y: number): Promise<void>;
+    /**
+    * @param x Transpied from float
+    * @param y Transpied from float
+    * @returns Transpied from System.Threading.Tasks.Task
+    */
     onWriteLine(x: number, y: number): Promise<void>;
 }
 
